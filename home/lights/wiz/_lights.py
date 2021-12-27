633aba54d52592bc331a0a15b792b652cbcbe371
h0lybyte <admin@kbve.com>
2021-12-27 17:49:08 -0500

    Revision of .env and .kbve files
    Revision of .env and .kbve files

--- home/lights/wiz/_lights.py
+++ home/lights/wiz/_lights.py
@@ -1,21 +1,45 @@
+# Imports
 import asyncio
 import sys
+import os
+from pathlib import Path
+
+
+# Currently Fixing .env and .kbve files for Python.
+
 sys.path.append("C:\Python38\Lib\site-packages")
 
 from pywizlight import wizlight, PilotBuilder, discovery
 
-async def main():
-    """Sample code to work with bulbs."""
-    print(f"Testing Code for _lights.py")
-    # Discover all bulbs in the network via broadcast datagram (UDP)
-    # function takes the discovery object and returns a list with wizlight objects.
-    bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
-    # Print the IP address of the bulb on index 0
-    print(f"Bulb IP address: {bulbs[0].ip}")
 
-    # Iterate over all returned bulbs
-    for bulb in bulbs:
-        print(bulb.__dict__)
+#from dotenv import load_dotenv
+#dotenv_path = Path('../../../.env')
+#load_dotenv(dot_env_path=dotenv_path)
+
+
+
+
+
+async def hex_to_rgb(hexvalue):
+    hexvalue = hexvalue.lstrip("-#")
+    hexvalue = tuple(int(hexvalue[i:i+2], 16) for i in (0, 2, 4))
+    print(hexvalue)
+    return hexvalue
+
+# Code Based Upon Sbidy's Example
+async def main():
+    command = sys.argv[1]
+    if "-#" in command:
+        color = await hex_to_rgb(command)
+        bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
+        for bulb in bulbs: light = await wizlight(bulb.ip).turn_on(PilotBuilder(rgb = color))
+
+    if "-off" in command:
+        bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
+        for bulb in bulbs: light = await wizlight(bulb.ip).turn_off()
+    
+
+        
 
 
 loop = asyncio.get_event_loop()
